0da1a26ff214bf7707783533477e130fc2e75454
joerdsonsilva <75455785+joerdsonsilva@users.noreply.github.com>
2024-10-29 00:03:08 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
+ # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+ # SPDX-License-Identifier: Apache-2.0
 
 import cocotb
 from cocotb.clock import Clock
